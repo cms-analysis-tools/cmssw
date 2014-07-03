6faae2be1bff596957b64024e31a934dad30b263
Norraphat <srimanob@mail.cern.ch>
2014-07-03 03:00:18 +0200

    Exercise 10

---
+++ PhysicsTools/PatExamples/test/analyzePatTriggerPrescale_cfg.py
@@ -0,0 +1,22 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process( "TEST" )
+
+process.load( "FWCore.MessageService.MessageLogger_cfi" )
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool( False )
+)
+
+process.source = cms.Source( "PoolSource",
+    fileNames = cms.untracked.vstring(
+    )
+)
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32( -1 )
+)
+
+process.TFileService = cms.Service( "TFileService",
+    fileName = cms.string( 'analyzePatTriggerPrescale.root' )
+)
+
+process.p = cms.Path()
